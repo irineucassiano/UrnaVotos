fd36ac477a1eaa2e60eae24ad8cae4ccbc2934e8
Irineu Cassiano Neto <129699200+irineucassiano@users.noreply.github.com>
2023-08-06 19:48:45 -0300

    Correções + adição de voto nulo e em branco
    Corrigi alguns erros de retorno do script e adicionei um sistema para computar votos nulos e votos em branco.

--- js/script.js
+++ js/script.js
@@ -2,52 +2,78 @@
 let cxVoto = document.querySelector('#votoUsuario')
 let aviso = document.querySelector('#aviso')
 let formulario = document.querySelector('form')
+
+let fotoL = document.querySelector('#lula_img')
+let fotoB = document.querySelector('#bolsonaro_img')
+let figB = document.querySelector('#figB')
+let figL = document.querySelector('#figL')
 //coletando botoes
 let btnVotar = document.querySelector('#btnEnviar')
 let btnLimpar = document.querySelector('#btnLimpar')
 let btnVencedor = document.querySelector('#fim')
+let btnAnular = document.querySelector('#anular')
 
 //declarando variavel dos votos para cada candidato
 let votoL = 0
 let votoB = 0
+let votoN = 0
+let votoBr = 0
 let vencedor = ''
 
 
-//function verificar se voto é valido
+//calcular voto nulo 
+btnAnular.addEventListener('click', function(){
+    votoN += 1
+    console.log(votoN)
+})
+
+//function verificar se voto é branco
 function verificaVoto(){
     numVoto = parseFloat(cxVoto.value)
-    if (numVoto != 13 || numVoto !=22 ) {
-        aviso.textContent = 'Número invalido. Observe que os unicos numeros válidos são 13 e 22'
+    if (numVoto != 13 && numVoto !=22) {
+        votoBr += 1
+        return votoBr
     } else {
-        console.log('Voto valido')
+        console.log('Normalidade')
     }
-}
+    }
+
 
 //functio calcular voto
 //uso do switch proposital.
 btnVotar.addEventListener('click', function(e){
     numVoto = parseFloat(cxVoto.value)
+    numVotoN = parseFloat(votoN.valueOf)
 
     switch (numVoto){
         case 13:
-            votoL = votoL + 1
+            votoL += 1
             console.log(votoL)
             aviso.textContent = 'Voto computado. Você votou no candidato Lula'
         break
         
         case 22:
-            votoB = votoB + 1
+            votoB += 1
             console.log(votoB)
             aviso.textContent = 'Voto computado. Você votou no candidato Bolsonaro'
         break    
+        case 0:
+            votoN += 1
+            console.log(votoN)
+        break    
     }
-    console.log(votoL, votoB)
+    console.log(votoL, votoB, votoN, votoBr)
 })
 
 //configurando btnLimpar
 
 btnLimpar.addEventListener('click', function(){
     formulario.reset()
+    aviso.textContent = ''
+    fotoB.classList.remove('perdedor')
+    fotoL.classList.remove('perdedor')
+    figB.classList.remove('perdedor')
+    figL.classList.remove('perdedor')
 })
 
 //calculando vencedor 
@@ -57,16 +83,38 @@ btnVencedor.addEventListener('click', function(){
     if (votoL > votoB){
         console.log('Vencedor foi o cadido Lula com '+votoL+' votos, enquanto o candidado Bolsonaro teve '+votoB+' votos')
         vencedor = 'Luiz Inacio Lula da Silva'
+        fotoB.classList.add('perdedor')
+        figB.classList.add('perdedor')
+        console.log('Vencedor '+vencedor)
+        console.log('Votos nulos '+votoN)
+        console.log('Votos em branco '+votoBr)
+        console.log('Votos Lula '+votoL)
+        console.log('Votos Bolsonaro '+votoB)
         votoB = 0
         votoL = 0
     } else if(votoB > votoL) {
         console.log('O vencedor foi o candidato Bolsonaro com ' +votoB+ ' votos, enquanto o candidadoto lula teve '+votoL+ ' votos')
         vencedor = 'Jair Messias Bolsonaro'
+        fotoL.classList.add('perdedor')
+        figL.classList.add('perdedor')
+        console.log('Vencedor '+vencedor)
+        console.log('Votos nulos '+votoN)
+        console.log('Votos em branco '+votoBr)
+        console.log('Votos Lula '+votoL)
+        console.log('Votos Bolsonaro '+votoB)
         votoB = 0
         votoL = 0
     } else {
+        vencedor = 'Luiz Inacio Lula da Silva '
         console.log('Houve um empate. Por isso com base na lei nº 4.737/65 do artigo 110 do Código Eleitoral o candidato mais velho será eleito. Nesse caso o candidato Lula ganha a eleição')
+        console.log('Vencedor '+vencedor)
+        console.log('Votos nulos '+votoN)
+        console.log('Votos em branco '+votoBr)
+        console.log('Votos Lula '+votoL)
+        console.log('Votos Bolsonaro '+votoB)
         votoB = 0
         votoL = 0
     }
+ 
 })
+
